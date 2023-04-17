f14e00f16e2d390df72c456e172fd60dd32eeeaf
user <sktjsvy2@naver.com>
2023-04-17 15:35:02 +0900

    [jsp] 서블릿 get

---
+++ jspEx/src/main/java/ch08/GetServlet.java
@@ -0,0 +1,33 @@
+package ch08;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class GetServlet
+ */
+@WebServlet("/ch08/GetServlet")
+public class GetServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String msg = request.getParameter("msg");
+		response.setContentType("text/html; charset=EUC-KR");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Get Servlet 방식</h1>");
+		out.println("<h2>msg : "+msg + "</h2>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+       
+ 
+
+}
