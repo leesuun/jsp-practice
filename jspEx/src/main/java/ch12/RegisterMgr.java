c58b42846abb38637e9b4e2d789e31243e00da6a
user <sktjsvy2@naver.com>
2023-04-25 10:54:28 +0900

    [jsp] 세션 로그인

--- jspEx/src/main/java/ch12/RegisterMgr.java
+++ jspEx/src/main/java/ch12/RegisterMgr.java
@@ -6,37 +6,37 @@ import java.sql.ResultSet;
 
 import ch11.DBConnectionMgr;
 
-public class RegisterMgr{
-	
+public class RegisterMgr {
+
 	private DBConnectionMgr pool;
 
-	public RegisterMgr(){
-		try{
+	public RegisterMgr() {
+		try {
 			pool = DBConnectionMgr.getInstance();
-		}catch(Exception e){
-			System.out.println("Error : 커넥션 연결 실패");			
+		} catch (Exception e) {
+			System.out.println("Error : 커넥션 연결 실패");
 		}
 	}
 
 	public boolean loginRegister(String id, String pwd) {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        boolean loginCon = false;
-        try {
-            con = pool.getConnection();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		boolean loginCon = false;
+		try {
+			con = pool.getConnection();
 			String query = "select count(*) from customer_2 where id = ? and pwd = ?";
-            pstmt = con.prepareStatement(query);
-            pstmt.setString(1, id);
-            pstmt.setString(2, pwd);
-            rs = pstmt.executeQuery();
-            if(rs.next()&&rs.getInt(1)>0) 
-            	loginCon =true;
-        }catch(Exception ex) {
-            System.out.println("Exception" + ex);
-        }finally{
-             pool.freeConnection(con,pstmt,rs);
-        }
-        return loginCon;
-    }
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pwd);
+			rs = pstmt.executeQuery();
+			if (rs.next() && rs.getInt(1) > 0)
+				loginCon = true;
+		} catch (Exception ex) {
+			System.out.println("Exception" + ex);
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return loginCon;
+	}
 }
---
+++ jspEx/src/main/java/ch12/RegisterMgr3.java
@@ -0,0 +1,85 @@
+package ch12;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import ch11.DBConnectionMgr;
+import ch11.RegisterBean;
+
+public class RegisterMgr3 {
+
+	private DBConnectionMgr pool;
+	Connection con = null;
+	PreparedStatement pstmt = null;
+	Statement stmt = null;
+	ResultSet rs = null;
+	RegisterBean regBean = null;
+
+	public RegisterMgr3() {
+		try {
+			pool = DBConnectionMgr.getInstance();
+			/* con = pool.getConnection(); */
+			/* System.out.println("con: " + con); */
+		} catch (Exception e) {
+			System.out.println("Error : 커넥션 연결 실패");
+		}
+	}
+
+	public RegisterBean getName(String id) throws SQLException {
+		regBean = new RegisterBean();
+
+		/* String strQuery = "select name from customer_2 where id= "+"'"+ id +"'"; */
+		String strQuery = "select name from customer_2 where id=" + "'" + id + "'";
+		System.out.println(strQuery);
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(strQuery);
+			while (rs.next()) {
+				regBean.setName(rs.getString("name"));
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
+
+		return regBean;
+	}
+
+	public boolean loginRegister(String id, String pwd) {
+
+		boolean loginCon = false;
+		try {
+			con = pool.getConnection();
+			String query = "select count(*) from customer_2 where id = ? and pwd = ?";
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pwd);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next() && rs.getInt(1) > 0) {
+				loginCon = true;
+			}
+
+		} catch (Exception ex) {
+			System.out.println("Exception" + ex);
+		} finally {
+			try {
+				pool.freeConnection(con, pstmt, rs);
+				/*
+				 * pstmt.close(); rs.close();
+				 */
+			} catch (Exception e) {
+				// TODO: handle exception
+
+			}
+
+		}
+		System.out.println(loginCon);
+		return loginCon;
+	}
+}
